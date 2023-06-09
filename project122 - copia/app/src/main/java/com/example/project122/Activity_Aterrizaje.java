c51ee2101c55782907931276e3d6090739b763c4
Soret1516 <117451548+Soret1516@users.noreply.github.com>
2023-06-09 15:56:06 -0500

    Se agrega modal a la vista de aterrizaje

--- project122 - copia/app/src/main/java/com/example/project122/Activity_Aterrizaje.java
+++ project122 - copia/app/src/main/java/com/example/project122/Activity_Aterrizaje.java
@@ -3,13 +3,69 @@ package com.example.project122;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class Activity_Aterrizaje extends AppCompatActivity {
-    //private ConstraintLayout bottomsheet;
+    private ConstraintLayout bottomsheet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aterrizaje);
+        bottomsheet = findViewById(R.id.botttom_sheet);
+        bottomsheet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                showDialog();
+
+            }
+        });
+
+    }
+
+    private void showDialog() {
+
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.bottomsheetlayout);
+
+        LinearLayout editLayout = dialog.findViewById(R.id.layoutEdit);
+        LinearLayout shareLayout = dialog.findViewById(R.id.layoutShare);
+
+        editLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialog.dismiss();
+                Toast.makeText(Activity_Aterrizaje.this,"Usuario seleccionado",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        shareLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialog.dismiss();
+                Toast.makeText(Activity_Aterrizaje.this,"Proveedor seleccionado",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
     }
 }
