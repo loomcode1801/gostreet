7aa472bc997370f4ca36d0690832a9402774216c
Santiago Alvarez Porras <santiagoalvarezporras@gmail.com>
2023-06-12 02:07:54 -0500

    se crea funcionalidad del uso del recycleview con firebase

--- ejemplos/ejemploFirebase/pruebas/app/src/main/java/com/example/pruebas/MyAdapter.java
+++ ejemplos/ejemploFirebase/pruebas/app/src/main/java/com/example/pruebas/MyAdapter.java
@@ -1,6 +1,7 @@
 package com.example.pruebas;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -16,7 +17,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     Context context;
 
-    ArrayList<Luchador> list;  // simulacion de la clase User
+    ArrayList<Luchador> list;  // simulacion de la clase User en este caso luchador
 
 
     public MyAdapter(Context context, ArrayList<Luchador> list) {
@@ -27,25 +28,35 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(context).inflate(R.layout.item,parent, false);   // se modifica este
+        return new MyViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
 
+        Luchador luchador = list.get(position);
+
+        holder.id.setText(String.valueOf(luchador.getId() + "prueba"));
+        holder.nombre.setText(luchador.getNombre());
+
+
     }
 
     @Override
     public int getItemCount() {
-        return list.size();
+        return list.size();   // se modifica este
     }
 
     public  static  class  MyViewHolder extends  RecyclerView.ViewHolder{
 
-        TextView id, nombre; 
+        TextView id, nombre;    // se modifica este
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            id = itemView.findViewById(R.id.tvfirstName);       //se modifica estos dos
+            nombre = itemView.findViewById(R.id.tvlastName);
         }
     }
 
